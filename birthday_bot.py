5843ae665b90ec338d0b4289a0285ed98a5cf9de
Daniel Schjetne <71273660+danielschjetne@users.noreply.github.com>
2021-06-10 23:47:17 +0200

    final bot
    fully functional bot, ran on crontab

--- birthday_bot.py
+++ birthday_bot.py
@@ -1,48 +1,51 @@
-# Importing libraries
 from slack_sdk import WebClient
-from dotenv import load_dotenv
 import datetime as dt
 import attr
 import csv
 import os
 
-# Defining the Birthday-class.
+
 @attr.s(auto_attribs=True, frozen=True)
 class Birthday:
     name: str
     date_of_birth_string: str
     user_id: str
 
+    def congratulation_string(self):
+        return f'Happy birthday to {self.name}! {self.user_id}! :balloon: :cake:'
 
-# Reading the .csv-file
+    def is_celebrated(self, date):
+        return date == self.date_of_birth_string
+
+
 def read_csv(file_name):
     with open(file_name, 'r') as birthdays:
         birthday_list = []
-    
+
         csv_reader = csv.reader(birthdays, delimiter=',')
-        next(csv_reader)  # Getting rid of headers
-        
-        # Looping through rows and appends birthdays to the birthday list. 
+        next(csv_reader)  # Remove headers
+
         for row in csv_reader:
             name, date, month, user_id = row
             birthday = Birthday(name, f'{date}-{month}', user_id)
             birthday_list.append(birthday)
+
     return birthday_list
 
 
-# Check if there is a birthday today
 def birthday_bot(birthdays, slack_token):
+    """
+    Check if there is a birthday today
+    """
     today = f'{dt.date.today():%d-%m}'
-    slack_client = WebClient(slack_token)  # Slack API OAuth token
-    for date_of_birth in birthdays:
-        # If birthday == True: post message!
-        if today == date_of_birth.date_of_birth_string:
-            message = f'Happy birthday to {date_of_birth.name}! {date_of_birth.user_id}! :balloon: :cake:'
-            slack_client.chat_postMessage(channel=channel, text=message)
+    slack_client = WebClient(token=slack_token)  # OAuth token
+    for birthday in birthdays:
+        if birthday.is_celebrated(today):
+            slack_client.chat_postMessage(
+                channel=channel, text=birthday.congratulation_string())
 
 
 if __name__ == '__main__':
-    load_dotenv()  # Loading token and channel from .env file
-    token = os.environ.get('SLACK_BOT_TOKEN')
-    channel = os.environ.get('BIRTHDAY_BOT_CHANNEL')  # Channel to post to
+    token = os.environ['SLACK_BOT_TOKEN']
+    channel = os.environ['BIRTHDAY_BOT_CHANNEL'] 
     birthday_bot(birthdays=read_csv('birthdays.csv'), slack_token=token)
